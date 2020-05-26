2f61019d6da350ce3e577b5115e82b727b1c7112
philips77 <aleksander.nowakowski@nordicsemi.no>
2020-05-26 15:32:02 +0200

    Missing retention policy annotations

--- dfu/src/main/java/no/nordicsemi/android/dfu/DfuScope.java
+++ dfu/src/main/java/no/nordicsemi/android/dfu/DfuScope.java
@@ -2,7 +2,11 @@ package no.nordicsemi.android.dfu;
 
 import androidx.annotation.IntDef;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 @SuppressWarnings("WeakerAccess")
+@Retention(RetentionPolicy.SOURCE)
 @IntDef(value = {
             DfuServiceInitiator.SCOPE_SYSTEM_COMPONENTS,
             DfuServiceInitiator.SCOPE_APPLICATION
