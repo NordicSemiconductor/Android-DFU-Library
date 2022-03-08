017b01206328c67cf047eb362886864cf6df2798
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2022-03-08 11:54:03 +0100

    Add deeplink example

--- lib_storage/src/main/java/no/nordicsemi/dfu/storage/DeepLinkHandler.kt
+++ lib_storage/src/main/java/no/nordicsemi/dfu/storage/DeepLinkHandler.kt
@@ -1,8 +1,12 @@
 package no.nordicsemi.dfu.storage
 
 import android.content.Intent
-import android.util.Log
 import javax.inject.Inject
+
+/**
+ * Example test: adb shell am start -W -a android.intent.action.VIEW -d "http://www.nordicsemi.com/dfu/?file=https://drive.google.com/uc?export=download%26id=1EcPZBA7Mi4-g-ygnk1SnTiSm0r57i1Rt" no.nordicsemi.android.dfu
+ * '&' in nested link encode with '%26'
+ */
 
 private const val PARAM_KEY = "file"
 
