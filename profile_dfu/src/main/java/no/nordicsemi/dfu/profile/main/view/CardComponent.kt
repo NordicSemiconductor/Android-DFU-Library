627b1625e271306e24a75baba963287ae985411b
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2022-03-17 13:05:54 +0100

    Revert "Add Ukraine support"
    This reverts commit e06346b463a6a96a2e45b6207590d1fa86b4a6c4.

--- profile_dfu/src/main/java/no/nordicsemi/dfu/profile/main/view/CardComponent.kt
+++ profile_dfu/src/main/java/no/nordicsemi/dfu/profile/main/view/CardComponent.kt
@@ -1,7 +1,6 @@
 package no.nordicsemi.dfu.profile.main.view
 
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -11,10 +10,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import no.nordicsemi.dfu.profile.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -39,7 +36,6 @@ internal fun CardComponent(
             horizontalArrangement = Arrangement.Start,
             modifier = Modifier.padding(16.dp)
         ) {
-
             Box(
                 modifier = Modifier
                     .size(40.dp)
@@ -48,14 +44,6 @@ internal fun CardComponent(
                         shape = CircleShape
                     )
             ) {
-                Image(
-                    painter = painterResource(R.drawable.ic_ukraine_flag),
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .size(40.dp)
-                        .clip(CircleShape)
-                )
                 Icon(
                     painter = painterResource(id = titleIcon),
                     contentDescription = null,
