a4f848e80a4361ccc98b839cc9c3eb0c05805ce2
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2023-10-03 12:54:58 +0200

    A preview for the Welcome screen

--- profile/welcome/src/main/java/no/nordicsemi/android/dfu/profile/welcome/view/WelcomeScreen.kt
+++ profile/welcome/src/main/java/no/nordicsemi/android/dfu/profile/welcome/view/WelcomeScreen.kt
@@ -47,6 +47,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -115,4 +116,10 @@ internal fun WelcomeScreen() {
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun WelcomeScreenPreview() {
+    WelcomeScreen()
 }
